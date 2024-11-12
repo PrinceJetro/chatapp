5bfd11e8a11a9dd1f11684edc3e6acaf69deba63
PrinceJetro <adegbuyijephthah@gmail.com>
2024-11-12 14:18:59 +0100

    changed the view stuff

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', getRoutes, name='register'),
     path('all', getAll, name='all'),
     path('create', createComplaint, name='create'),
-    path('student', createStudent, name='student'),
+    path('student', send_contact_email, name='student'),
 ]
